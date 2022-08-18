3d56b6786f0f891096d4adfdf162a72e379ae696
oott123 <git@public.oott123.com>
2022-08-18 22:37:40 +0800

    chore: print request url for debug

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -10,11 +10,11 @@ export const onRequest = [
     const srcUrl = request.url
     const [ domain, uri ] = srcUrl.match(/^\/api\/([^/]+)(.*)$/) || []
     if (!domain || !uri) {
-      return new Response(`404 not found\r\ndomain: ${domain}\r\nuri: ${uri}`, { status: 404 })
+      return new Response(`404 not found\r\ndomain: ${domain}\r\nuri: ${uri}\r\nrequest url: ${request.url}`, { status: 404 })
     }
     const baseUrl = baseUrlMap[domain]
     if (!baseUrl) {
-      return new Response(`404 base url not found\r\ndomain: ${domain}\r\nuri: ${uri}`, { status: 404 })
+      return new Response(`404 base url not found\r\ndomain: ${domain}\r\nuri: ${uri}\r\nrequest url: ${request.url}`, { status: 404 })
     }
     const newUrl = new URL(uri, baseUrl)
     const newRequest = new Request(newUrl, request)
